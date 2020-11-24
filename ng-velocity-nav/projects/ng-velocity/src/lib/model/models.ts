aeb3997fafa6099f3f790965222498f3ad60ca3d
Eric <ericsarpong@hotmail.com>
2020-11-23 23:01:48 -0500

    initialization of project

---
+++ ng-velocity-nav/projects/ng-velocity/src/lib/model/models.ts
@@ -0,0 +1,28 @@
+import { NavigationExtras } from '@angular/router';
+
+export interface NavigationResponse {
+    isSuccess: boolean;
+    isError: boolean;
+    errorMessage?: string;
+}
+
+export interface NavAuxiliary {
+    destinationPage?: string;
+    navigationExtra?: NavigationExtras;
+    preprocess?: Function;
+}
+
+export class NavAux {
+    destinationPage: string;
+    navigationExtra: NavigationExtras;
+    preprocess: Function;
+    params: any;
+
+    constructor(destinationPage?: string, navigationExtra?: NavigationExtras | undefined, preprocess?: Function, param?: any) {
+        this.destinationPage = destinationPage;
+        this.navigationExtra = navigationExtra;
+        this.preprocess = preprocess;
+        this.params = param;
+    }
+}
+
---
+++ ng-velocity-nav/projects/ng-velocity/src/public_api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of ng-velocity
+ */
+
+import { NavigationService } from './lib/navigation/navigation.service';
+
+export * from './lib/ng-velocity.module';
+export { RouteNext } from './lib/decorators/nav-decorators';
+export { NavAux } from './lib/model/models';
+export { NavigationService } from './lib/navigation/navigation.service';
+
+
