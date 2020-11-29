7e2a25917a1357bdad965474bdca28da0cfe5a7a
Eric <ericsarpong@hotmail.com>
2020-11-28 21:52:54 -0500

    exposing new functions

--- ng-velocity-nav/projects/ng-velocity/src/public_api.ts
+++ ng-velocity-nav/projects/ng-velocity/src/public_api.ts
@@ -6,6 +6,8 @@ import { NavigationService } from './lib/navigation/navigation.service';
 
 export * from './lib/ng-velocity.module';
 export { RouteNext } from './lib/decorators/nav-decorators';
+export { RouteBack } from './lib/decorators/nav-decorators';
+export { RouteToState } from './lib/decorators/nav-decorators';
 export { NavAux } from './lib/model/models';
 export { NavigationService } from './lib/navigation/navigation.service';
 
