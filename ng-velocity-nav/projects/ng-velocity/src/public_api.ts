82d6ab234af78c6367d36977bfa8e19cf2d6f952
Eric <ericsarpong@hotmail.com>
2020-12-05 23:16:37 -0500

    added new apis

--- ng-velocity-nav/projects/ng-velocity/src/public_api.ts
+++ ng-velocity-nav/projects/ng-velocity/src/public_api.ts
@@ -8,6 +8,9 @@ export * from './lib/ng-velocity.module';
 export { RouteNext } from './lib/decorators/nav-decorators';
 export { RouteBack } from './lib/decorators/nav-decorators';
 export { RouteToState } from './lib/decorators/nav-decorators';
+export { RouteNextAsync } from './lib/decorators/nav-decorators';
+export { RouteBackAsync } from './lib/decorators/nav-decorators';
+export { RouteToStateAsync } from './lib/decorators/nav-decorators';
 export { NavAux } from './lib/model/models';
 export { NavigationService } from './lib/navigation/navigation.service';
 
