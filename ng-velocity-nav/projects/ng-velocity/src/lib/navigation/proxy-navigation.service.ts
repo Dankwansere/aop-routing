c11f4cd7d268f17c64e5df8066ec0ca8ea13a54b
Eric Sarpong <ericsarpong@hotmail.com>
2020-12-16 01:44:00 -0500

    Feat/error and custom nav (#15)
    * created utility functions
    
    * implemented error handling
    
    * added error enums
    
    * created abstract base navigation class
    
    * proxy navigation class to be used as proxy for custom logic
    
    * exporting new functions

---
+++ ng-velocity-nav/projects/ng-velocity/src/lib/navigation/proxy-navigation.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { NavAux } from '../model/models';
+
+@Injectable()
+/**
+ * Dummy service to allow custom implemenation of navigation logic.
+ * Should be overriden by a custom navigation class inheriting BaseNavigation Class.
+ * The custom navigation class should be provided by the angular injector through the `useClass` attribute
+ * of the provider array.
+ */
+export class ProxyNavigationService {
+    goToNextPage(navObj: NavAux, ...args: any[]): void {}
+    goToPreviousPage(navObj: NavAux, ...args: any[]): void {}
+    goToState(navObj: NavAux, ...args: any[]): void {}
+}
