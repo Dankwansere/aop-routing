787b2516f187cdcc2af842be219b97617200ed40
Eric <ericsarpong@hotmail.com>
2020-12-16 01:25:48 -0500

    created abstract base navigation class

--- ng-velocity-nav/projects/ng-velocity/src/lib/model/models.ts
+++ ng-velocity-nav/projects/ng-velocity/src/lib/model/models.ts
@@ -29,4 +29,18 @@ export class NavAux {
     }
 }
 
+export abstract class BaseNavigation {
+
+
+    /**
+     * Skeleton methods to be overrided and provide custom navigation logic.
+     * Recommended to utilize NavAux class, as this class encapsulates navigation properties and will be passed
+     * by the decorator functions.
+     * @param navObj - Encapsulates navigation properties
+     */
+    abstract goToNextPage(navObj: NavAux, ...args): void;
+    abstract goToPreviousPage(navObj: NavAux, ...args): void;
+    abstract goToState(navObj: NavAux, ...args): void;
+}
+
 
