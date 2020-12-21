1d5ceb3662668a8323814cc5efab7783c0744d02
Eric <ericsarpong@hotmail.com>
2020-12-21 00:57:10 -0500

    added routeTransform interface

--- ng-velocity-nav/projects/ng-velocity/src/lib/model/models.ts
+++ ng-velocity-nav/projects/ng-velocity/src/lib/model/models.ts
@@ -1,4 +1,6 @@
 import { NavigationExtras } from '@angular/router';
+import { Type } from '@angular/core';
+
 
 export interface NavigationResponse {
     isSuccess: boolean;
@@ -12,25 +14,23 @@ export interface NavAuxiliary {
     preprocess?: Function;
 }
 
+export interface RouteTransform {
+    path: string;
+    component?: Type<any>;
+    canActivateGuards?: [];
+}
+
 /**
  * Class to encapsulate extra navigation properties or methods that's required to execute the navigation process.
  */
 export class NavAux {
-    destinationPage: string | number;
-    navigationExtra: NavigationExtras;
-    preprocess: Function;
-    params: any;
-
-    constructor(destinationPage?: string | number, navigationExtra?: NavigationExtras | undefined, preprocess?: Function, param?: any) {
-        this.destinationPage = destinationPage;
-        this.navigationExtra = navigationExtra;
-        this.preprocess = preprocess;
-        this.params = param;
+    constructor(public destinationPage?: string | number,
+         public navigationExtra?: NavigationExtras | undefined,
+         public preprocess?: Function, public param?: any) {
     }
 }
 
 export abstract class BaseNavigation {
-
 
     /**
      * Skeleton methods to be overrided and provide custom navigation logic.
