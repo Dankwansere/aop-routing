8797e373db5f434bceaad92276d127e9634698fb
Eric <ericsarpong@hotmail.com>
2020-12-27 01:12:10 -0500

    added new public objects

--- aop-navigation/projects/aop-routing/src/public_api.ts
+++ aop-navigation/projects/aop-routing/src/public_api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of ng-velocity
  */
 
-export * from './lib/ng-velocity.module';
+export * from './lib/aop-routing.module';
 export { RouteNext } from './lib/decorators/nav-decorators';
 export { RouteBack } from './lib/decorators/nav-decorators';
 export { RouteToState } from './lib/decorators/nav-decorators';
@@ -10,6 +10,8 @@ export { RouteNextAsync } from './lib/decorators/nav-decorators';
 export { RouteBackAsync } from './lib/decorators/nav-decorators';
 export { RouteToStateAsync } from './lib/decorators/nav-decorators';
 export { NavAux } from './lib/model/models';
+export { AopConfig } from './lib/model/models';
+export { AopNav } from './lib/model/models';
 export { RouteTransform } from './lib/model/models';
 export { NavigationService } from './lib/navigation/navigation.service';
 export { BaseNavigation } from './lib/model/models';
