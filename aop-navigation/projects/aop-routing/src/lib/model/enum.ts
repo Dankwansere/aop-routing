a15d4436ae3130545eb1f606c4edd720a55f7c29
Eric <ericsarpong@hotmail.com>
2020-12-27 22:03:47 -0500

    Refactor: Fixed grammar mistakes for error messages

--- aop-navigation/projects/aop-routing/src/lib/model/enum.ts
+++ aop-navigation/projects/aop-routing/src/lib/model/enum.ts
@@ -1,9 +1,10 @@
 export enum NavError {
     OBSERVABLE_REQUIRED = 'To use route async, method must return an observable<string | NavAux>',
-    OBSERVABLE_STREAM = 'encountered error in observable stream',
+    OBSERVABLE_STREAM = 'Encountered error in observable stream',
     UPDATING_NAVAUX = 'Error updating NavAux instance',
     PREPROCRESS_FUNC = 'An error occurred executing function ',
-    ROUTING = 'could not route to ',
-    LOCATION_BACK = 'failed to popState navigate',
-    STATE_HISTORY = 'failed to load specific page from session history'
+    ROUTING = 'Could not route to ',
+    LOCATION_BACK = 'Failed to popState navigate',
+    STATE_HISTORY = 'Failed to load specific page from session history',
+    EXPIREMENTAL_FEATURE_ROUTE_TRANSFORM = 'Cannot use RouteTransform object with experimental flag turned off'
 }
