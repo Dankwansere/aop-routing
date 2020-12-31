df7f536956d71d0a501422ba15062db462ac114e
Eric <ericsarpong@hotmail.com>
2020-12-31 01:11:46 -0500

    completed proxy navigation service unit tests

--- aop-navigation/projects/aop-routing/src/lib/navigation/proxy-navigation.service.spec.ts
+++ aop-navigation/projects/aop-routing/src/lib/navigation/proxy-navigation.service.spec.ts
@@ -1,13 +1,38 @@
+import { NavAux } from '../model/models';
+import { ProxyNavigationService } from './proxy-navigation.service';
 
 
-// describe('Utility', () => {
-//     beforeEach(() => {
-       
-//       });
+describe('ProxyNavigationService', () => {
+    const navAux = new NavAux('aop-nav');
+    beforeEach(() => {
 
-//       describe('#isTypeString', ()=> {
-//         it('should be created', () => {
-           
-//           });
-//      });
-// });
+      });
+
+      describe('#goToNextPage', () => {
+        it('should call goToNextPage', () => {
+            const proxy = new ProxyNavigationService();
+            spyOn(proxy, 'goToNextPage');
+            proxy.goToNextPage(navAux);
+            expect(proxy.goToNextPage).toHaveBeenCalledWith(navAux);
+          });
+    });
+
+    describe('#goToPreviousPage', () => {
+        it('should call goToPreviousPage', () => {
+            const proxy = new ProxyNavigationService();
+            spyOn(proxy, 'goToPreviousPage');
+            proxy.goToPreviousPage(navAux);
+            expect(proxy.goToPreviousPage).toHaveBeenCalledWith(navAux);
+          });
+    });
+
+    describe('#goToState', () => {
+        it('should call goToState', () => {
+            const proxy = new ProxyNavigationService();
+            spyOn(proxy, 'goToState');
+            proxy.goToState(navAux);
+            expect(proxy.goToState).toHaveBeenCalledWith(navAux);
+          });
+    });
+
+});
