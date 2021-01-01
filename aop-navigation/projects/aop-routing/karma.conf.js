ef846df7f3c416232f467e4ae1d722d4131bc907
Eric Sarpong <ericsarpong@hotmail.com>
2020-12-31 21:24:34 -0500

    Feat/unit test (#22)
    * utility unit test and jest set up
    
    * removing debugging logs
    
    * commenting out jest types to use jasmine
    
    * completed utility unit tests
    
    * added condition to check if navObj is defined
    
    * added mockFunction
    
    * completed unit tests for navigation-helper
    
    * upgraded @types/jasmine version
    
    * minor refactor
    
    * added new mock function
    
    * completed unit tests for NavigationService class
    
    * minor refactor
    
    * updated morckPartialRouter function return value
    
    * change method acccessors to public to be able to test
    
    * completed route helper
    
    * minor refactor to logic
    
    * added mock decorator functions
    
    * completed nav decorator unit tests
    
    * completed proxy navigation service unit tests
    
    * removed jest from types array

--- aop-navigation/projects/aop-routing/karma.conf.js
+++ aop-navigation/projects/aop-routing/karma.conf.js
@@ -7,25 +7,27 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-mocha-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      'karma-spec-reporter'
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'text-summary', 'json'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
